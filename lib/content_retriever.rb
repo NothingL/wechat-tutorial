a536c200428b160611c1795b81754a68a4d5a941
NothingL <562908590@qq.com>
2014-10-27 14:11:32 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -8,8 +8,8 @@ class ContentRetriver
 			contents: [{
 				title: '各国泡面敬请期待',
 				description: '功能还在开发中==',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://162.105.30.186/members/lichong/lichong.jpg'}]
+				picture: 'http://162.105.30.186/members/lichong/lichong.jpg',
+				url: 'http://162.105.30.186/members/lichong/index.html'}]
 		}
 	end
 end
